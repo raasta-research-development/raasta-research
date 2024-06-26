38ea33b8a9af29ee8c8a741ccde275961017ef6f
Takshak Mudgal <takshakmudgal@gmail.com>
2024-06-26 13:56:27 +0530

    about page hero section adjustments

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,71 +1,63 @@
-'use client'
-import * as React from "react"
-import Image from 'next/image'
+"use client";
+import * as React from "react";
+import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
 
-// import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
-
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
 
 function About() {
   const imageUrls = [
-    '/images/c1.webp',
-    '/images/c2.webp',
-    '/images/c3.webp',
-    '/images/c4.webp',
-    '/images/c5.webp'
+    "/images/c1.webp",
+    "/images/c2.webp",
+    "/images/c3.webp",
+    "/images/c4.webp",
+    "/images/c5.webp",
   ];
 
-
   return (
-    <div className="aboutmain mt-28 overflow-x-auto">
-      <div
-        className="logodiv flex flex-row justify-center items-start"
-        style={{ height: "220px" }}
-      >
-        <img
+    <div className="mt-28">
+      <div className="flex justify-center items-center gap-5">
+        <Image
           src="/images/foot.png"
           alt="News and Updates"
-          style={{ width: "75px", height: "150px" }}
+          width="40"
+          height="40"
+          className=" pb-5 "
         />
 
-         <Image
-        src="/images/raasta.png"
-        alt="logo"
-        width={300}  
-        height={200}  
-        layout="responsive"
-        objectFit="contain" 
-        className="object-cover rounded-lg w-80 h-72"
-      />
+        <Image src="/images/raasta.png" alt="logo" width="520" height="520" />
       </div>
 
-<Carousel
-         plugins={[
+      <Carousel
+        plugins={[
           Autoplay({
             delay: 2000,
           }),
         ]}
         opts={{
           align: "start",
-          loop: true, 
+          loop: true,
         }}
         className="ml-28 max-w-7xl"
       >
-                <CarouselContent className="max-h-96 h-96">
+        <CarouselContent className="max-h-96 h-96">
           {imageUrls.map((url, index) => (
             <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
               <div className="p-1 h-full">
                 <Card className="h-full ">
                   <CardContent className="flex aspect-square items-center justify-center p-0">
-                    <img src={url} alt={`Image ${index + 1}`} className="object-cover h-full w-full" />
+                    <img
+                      src={url}
+                      alt={`Image ${index + 1}`}
+                      className="object-cover h-full w-full"
+                    />
                   </CardContent>
                 </Card>
               </div>
@@ -74,21 +66,29 @@ function About() {
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
-
       </Carousel>
-
-
-      
 
       <div className="pr-44 pl-44 pt-6 bg-white text-center">
         <span className="text-lg">
-        <strong>Hello Global Community,</strong> 
-           <br />
-          Were Raasta Research and Development, embarking on an exciting journey to explore various facets of global sustainable development. In our dedicated quest, we're not only uncovering a wealth of knowledge available around the world but also ensuring it's freely accessible to everyone. Our goal is to bridge the gap in understanding nature and its resources, cultivating global awareness that encourages conscientious choices for a harmonious environment.
-          
-          Recognizing the intricate nature of sustainability, which varies across regions, we emphasize the need for tailored plans instead of a universal approach. This involves respecting the unique characteristics, challenges, and opportunities in each area. Our commitment extends to spotlighting the dedicated efforts of individuals and organizations worldwide tirelessly working towards sustainability.
-          
-          Through our platform, we aim to be a hub of information, connecting regional initiatives with global awareness. By sharing insights into sustainable development projects worldwide, we strive to contribute to a shared understanding and promotion of responsible and enduring practices.
+          <strong>Hello Global Community,</strong>
+          <br />
+          Were Raasta Research and Development, embarking on an exciting journey
+          to explore various facets of global sustainable development. In our
+          dedicated quest, we're not only uncovering a wealth of knowledge
+          available around the world but also ensuring it's freely accessible to
+          everyone. Our goal is to bridge the gap in understanding nature and
+          its resources, cultivating global awareness that encourages
+          conscientious choices for a harmonious environment. Recognizing the
+          intricate nature of sustainability, which varies across regions, we
+          emphasize the need for tailored plans instead of a universal approach.
+          This involves respecting the unique characteristics, challenges, and
+          opportunities in each area. Our commitment extends to spotlighting the
+          dedicated efforts of individuals and organizations worldwide
+          tirelessly working towards sustainability. Through our platform, we
+          aim to be a hub of information, connecting regional initiatives with
+          global awareness. By sharing insights into sustainable development
+          projects worldwide, we strive to contribute to a shared understanding
+          and promotion of responsible and enduring practices.
         </span>
         <button>Know more</button>
       </div>
