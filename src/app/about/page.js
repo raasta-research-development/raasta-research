15bf1bba916e18d5ed79b48b0504d0cf62222ae7
rohitp4nt <rohitpant90122@gmail.com>
2024-07-01 11:10:29 +0530

    changes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -101,39 +101,40 @@ function About() {
       </div>
 
       <Carousel
-        plugins={[
-          Autoplay({
-            delay: 2000,
-          }),
-        ]}
-        opts={{
-          align: "start",
-          loop: true,
-        }}
-        className="ml-28 max-w-7xl"
-      >
-        <CarouselContent className="max-h-96 h-96">
-          {imageUrls.map((url, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-              <div className="p-1 h-full">
-                <Card className="h-full ">
-                  <CardContent className="flex aspect-square items-center justify-center p-0">
-                    <img
-                      src={url}
-                      alt={`Image ${index + 1}`}
-                      className="object-cover h-full w-full"
-                    />
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+  plugins={[
+    Autoplay({
+      delay: 2000,
+    }),
+  ]}
+  opts={{
+    align: "start",
+    loop: true,
+  }}
+  className="ml-auto mr-auto md:ml-24 md:mr-0 max-w-7xl"
+>
+  <CarouselContent className="max-h-96 h-96">
+    {imageUrls.map((url, index) => (
+      <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+        <div className="p-1 h-full">
+          <Card className="h-full">
+            <CardContent className="flex aspect-square items-center justify-center p-0">
+              <img
+                src={url}
+                alt={`Image ${index + 1}`}
+                className="object-cover h-full w-full"
+              />
+            </CardContent>
+          </Card>
+        </div>
+      </CarouselItem>
+    ))}
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>
 
-      <div className="pr-44 pl-44 pt-6 bg-white text-center">
+
+      <div className=" pt-6 ml-72 bg-white text-center w-3/5">
         <span className="text-lg">
           <strong>Hello Global Community,</strong>
           <br />
@@ -155,27 +156,36 @@ function About() {
           projects worldwide, we strive to contribute to a shared understanding
           and promotion of responsible and enduring practices.
         </span>
-        <button>Know more</button>
-      </div>
+        <br></br>
+        <button className='border border-black text-lg px-3 py-1 mt-4'>
+  Know more
+</button> 
+</div>
 
       <div className=" ml-72 relative flex items-center w-5/6">
 
-  <h className="ml-20 absolute text-8xl font-bold tracking-tighter">How do we <br></br>
-  <span className="text-9xl">save</span> our <br /> planet ?</h>
+  <h className="ml-20 absolute text-8xl font-extrabold tracking-tighter" >How do we <br></br>
+  <h className="text-9xl ">SAVE</h> our <br /> planet ?</h>
   
   <video className="overflow-hidden ml-40" autoPlay loop muted>
-    <source src="/images/earth-loop.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
+  <source src="/images/earth-loop.mp4" type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
+
 </div>
 
-          <div className='flex'>
+          <div className='flex '>
       <div className='w-4/6'>
       <nav className='w-3/5 gap-2  ml-40'>
         <ul className='flex text-xl justify-between'>
-          <li  className="cursor-pointer hover:text-gray-500" onClick={() => setOption('option1')}>Planet Peep</li>
-          <li  className="cursor-pointer hover:text-gray-500" onClick={() => setOption('option2')}>Audio book</li>
-          <li  className="cursor-pointer hover:text-gray-500" onClick={() => setOption('option3')}>Articles & stories</li>
+        <li className="cursor-pointer hover:text-gray-500 border border-gray-300 rounded-md p-2 hover:bg-gray-50" onClick={() => setOption('option1')}>
+  Planet Peep
+</li>
+          <li className="cursor-pointer hover:text-gray-500 border border-gray-300 rounded-md p-2 hover:bg-gray-50" onClick={() => setOption('option2')}>
+Audio book</li>
+          <li className="cursor-pointer hover:text-gray-500 border border-gray-300 rounded-md p-2 hover:bg-gray-50" onClick={() => setOption('option3')}>
+  Articles & stories
+</li>
         </ul>
       </nav>
       <div>{renderContent()}</div>
@@ -189,49 +199,48 @@ function About() {
 </button>   
  </div>
     </div>
+    
     <Carousel
-        plugins={[
-          Autoplay({
-            delay: 2000,
-          }),
-        ]}
-        opts={{
-          align: "start",
-          loop: true,
-        }}
-        className="ml-28 mt-20 max-w-7xl"
-      >
-        <CarouselContent className="max-h-96 h-96">
-          {imageUrls.map((url, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-              <div className="p-1 h-full">
-                <Card className="h-full ">
-                  <CardContent className="flex aspect-square items-center justify-center p-0">
-                    <img
-                      src={url}
-                      alt={`Image ${index + 1}`}
-                      className="object-cover h-full w-full"
-                    />
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+  plugins={[
+    Autoplay({
+      delay: 2000,
+    }),
+  ]}
+  opts={{
+    align: "start",
+    loop: true,
+  }}
+  className="ml-auto mt-40 mr-auto md:ml-24 md:mr-0 max-w-7xl"
+>
+  <CarouselContent className="max-h-96 h-96">
+    {imageUrls.map((url, index) => (
+      <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+        <div className="p-1 h-full">
+          <Card className="h-full">
+            <CardContent className="flex aspect-square items-center justify-center p-0">
+              <img
+                src={url}
+                alt={`Image ${index + 1}`}
+                className="object-cover h-full w-full"
+              />
+            </CardContent>
+          </Card>
+        </div>
+      </CarouselItem>
+    ))}
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>
 
 
-      <div className='mt-20 flex flex-row mx-auto md:w-1/2 md:px-0.5'>
-  <h1 className='text-4xl md:text-8xl font-serif'>Why <br className="md:hidden" /> Raasta ?</h1>
-  <p className='text-base md:text-lg'>
+
+<div className="mt-20 mb-20 items-center gap-20 flex mx-auto md:w-3/5 md:px-4">
+  <h1 className="text-xl md:text-6xl leading-tight font-bold">Why Raasta?</h1>
+  <p className="text-base text-center md:text-lg leading-relaxed">
     In a world dealing with a range of environmental issues, it is crucial to have thorough information on sustainable development from diverse environmental, geographical, economical, and cultural perspectives. Recognizing that sustainability is not a one-size-fits-all concept. Each environment, geography, economy, and culture presents unique challenges, opportunities, and solutions. Through the exploration of global information, we gain an elaborate understanding of the diverse approaches to sustainability. This knowledge empowers us to tailor strategies that consider local contexts, ensuring the effectiveness of initiatives. Creating awareness worldwide is crucial because it fosters a collective understanding of the global interconnectedness of sustainable practices. It inspires collaboration, allowing the global population to learn from each other, adopt successful models, and collectively work towards a more sustainable and resilient future. In essence, the need to gather and share this information globally is essential for fostering a united front in the global pursuit of sustainability.
   </p>
 </div>
-
-
-
 
   </div>
 
