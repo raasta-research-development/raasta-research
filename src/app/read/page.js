412939bea8a74fed9977b2b2d0920e48a1703875
Takshak Mudgal <takshakmudgal@gmail.com>
2024-08-01 05:00:41 +0530

    feat: read page

---
+++ src/app/read/page.js
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import { HeaderWrapper } from "@/components/ui/header-wrapper";
+import { ArticleCards } from "@/components/ArticleCards";
+
+export default function Read() {
+  return (
+    <>
+      <HeaderWrapper />
+      <ArticleCards />
+      <Footer />
+    </>
+  );
+}
